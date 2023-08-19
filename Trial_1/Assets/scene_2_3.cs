3e3a7adba470bd823f4fd55f8c46cb44cc855ffc
Shiva9-lab <shivanshu.22011180@viit.ac.in>
2023-08-19 15:20:48 +0530

    created temporary UI

---
+++ Trial_1/Assets/scene_2_3.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene_2_3 : MonoBehaviour
+{
+    public void LoadSecondScene()
+    {
+        SceneManager.LoadScene("3rd_page");
+    }
+}
