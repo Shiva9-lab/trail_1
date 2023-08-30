5fd7498692b3948a0c80d9f2c1c3acdf2a543f85
Tanish-Raorane <tanishraorane4@gmail.com>
2023-08-30 19:22:37 +0530

    .

--- Trial_1/Assets/Name.cs
+++ Trial_1/Assets/Name.cs
@@ -2,14 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Name : MonoBehaviour
 {
-    public string input;
-  
-    public void StoreName(string s)
+    public TextMeshProUGUI text;
+    public InputField display;
+
+    private void Start()
     {
-        input = s;
-        Debug.Log(input);
+        text.text = PlayerPrefs.GetString("user_name");
+    }
+
+    public void StoreName()
+    {
+        text.text = display.text;
+        PlayerPrefs.SetString("user_name", text.text);
+        PlayerPrefs.Save();
     }
 }
--- Trial_1/Assets/NameScene3.cs
+++ Trial_1/Assets/NameScene3.cs
@@ -1,23 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
+//using TMPro;
 
-public class NameScene3 : MonoBehaviour
-{
-    Name n;
-    string display;
-    
-    public TextMeshProUGUI text;
+//public class NameScene3 : MonoBehaviour
+//{
+//    Name n;
+//    string display;
 
-    private void Start()
-    {
-        n = GetComponent<Name>();
-        display = n.input;
-        text.text = "Hey " + display;
-    }
+//    public TextMeshProUGUI text;
+
+//    private void Start()
+//    {
+//        n = GetComponent<Name>();
+//        display = n.input;
+//        text.text = "Hey " + display;
+//    }
 
 
 
-}
+//}
